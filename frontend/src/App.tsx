7ed19d3f159cb5248d23035cf415fd3caacd87a1
Autsada555 <autsaiheha@gmail.com>
2025-05-03 15:47:52 +0700

    update page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/navbar';
 import BookRoom from './pages/CalendarBooking';
 import UserProfile from './pages/UserProfile';
 import LoginPage from './pages/Login';
-import AdminDashboard from './pages/admin/AdminDashboard';
+// import AdminDashboard from './pages/admin/AdminDashboard';
 import DailyCheckForm from './pages/CheckCCTV';
 import Dashboard from './pages/DashBoard';
 
@@ -16,7 +16,7 @@ const App: React.FC = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/book" element={<BookRoom />} />
         <Route path="/profile" element={<UserProfile />} />
-        <Route path="/admindashboard" element={<AdminDashboard />} />
+        {/* <Route path="/admindashboard" element={<AdminDashboard />} /> */}
         <Route path="/checkcctv" element={<DailyCheckForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
         
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -35,7 +35,6 @@ const Navbar: React.FC = () => {
                 </div>
                 <ul className="py-2">
                   <li><Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600">Profile</Link></li>
-                  <li><Link to="/settings" className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600">Settings</Link></li>
                   <li><Link to="/logout" className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600">Sign out</Link></li>
                 </ul>
               </div>
@@ -59,9 +58,9 @@ const Navbar: React.FC = () => {
         <div className={`${menuOpen ? 'block' : 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1`} id="navbar-user">
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li><Link to="/dashboard" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0">Dashboard</Link></li>
-            <li><Link to="/book" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">Booking M</Link></li>
             <li><Link to="/checkcctv" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">CCTV</Link></li>
-            <li><Link to="/admindashboard" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">Admin Dashboard</Link></li>
+            <li><Link to="/book" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">Booking M</Link></li>
+            {/* <li><Link to="/admindashboard" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">Admin Dashboard</Link></li> */}
           </ul>
         </div>
       </div>
