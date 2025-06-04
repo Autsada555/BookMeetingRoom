d609720ce68e9be60bdb450c7c7443c4876de659
Autsada555 <autsadaheha@gmail.com>
2025-06-04 09:08:54 +0700

    update B

--- backend/middlewares/middlewares.go
+++ backend/middlewares/middlewares.go
@@ -14,17 +14,18 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		origin := c.Request.Header.Get("Origin")
-		allowedOrigins := []string{
-			"https://book-meeting-room-swart.vercel.app",
-			"https://bookmeetingroom-qk0h.onrender.com", // (ถ้าต้องการให้ backend เรียกตัวเองได้)
-		}
-		for _, o := range allowedOrigins {
-			if o == origin {
-				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-				break
-			}
-		}
+		// origin := c.Request.Header.Get("Origin")
+		// allowedOrigins := []string{
+		// 	"https://book-meeting-room-swart.vercel.app",
+		// 	"https://bookmeetingroom-qk0h.onrender.com", // (ถ้าต้องการให้ backend เรียกตัวเองได้)
+		// }
+		// for _, o := range allowedOrigins {
+		// 	if o == origin {
+		// 		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		// 		break
+		// 	}
+		// }
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://book-meeting-room-swart.vercel.app")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
